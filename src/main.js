8298a599443eca08ac7473883beb27a328abdd19
Dongseon Lee <eastline@maycan.co.kr>
2021-08-25 17:34:15 +0900

    Help Jimmy

--- src/main.js
+++ src/main.js
@@ -5,14 +5,6 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-import GAuth from "vue-google-oauth2";
-const gauthOption = {
-	clientId: "CLIENT_ID.apps.googleusercontent.com",
-	scope: "profile email",
-	prompt: "select_account",
-};
-Vue.use(GAuth, gauthOption);
-
 new Vue({
 	router,
 	store,
