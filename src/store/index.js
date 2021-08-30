890e252a13d8b657e847c4c5d5956d908cc2b382
silver <dkfvktorcp@naver.com>
2021-08-30 21:40:37 +0900

    little fix nextday button

--- src/store/index.js
+++ src/store/index.js
@@ -18,19 +18,42 @@ export default new Vuex.Store({
 		SET_NEXT_DAY(state) {
 			state.today = day.setDate(state.today + 1);
 			state.today = day.getDate();
+			state.weekDay = WEEKDAY[day.getDay() + 1];
 			if (state.today == 1) {
 				state.month += 1;
-			} else if (state.month == 13 && state.today == 1) {
-				state.year = day.setFullYear(state.year + 1);
-				state.year = day.getFullYear();
-				state.month = day.setMonth(state.month == 1);
-				state.month = day.getMonth() + 1;
+				if (state.month == 13) {
+					state.year = day.setFullYear(state.year + 1);
+					state.year = day.getFullYear();
+					state.month = day.setMonth(state.month == 1);
+					state.month = day.getMonth() + 1;
+				}
 			}
 		},
 		SET_PREVIOUS_DAY(state) {
 			state.today = day.setDate(state.today - 1);
 			state.today = day.getDate();
 			if (state.today == 31) {
+				if (
+					state.month == 1 ||
+					state.month == 3 ||
+					state.month == 5 ||
+					state.month == 7 ||
+					state.month == 8 ||
+					state.month == 10 ||
+					state.month == 12
+				) {
+					state.month -= 1;
+				} else if (state.month == 0) {
+					state.year = day.setFullYear(state.year) - 1;
+					state.year = day.getFullYear();
+					state.month = day.setMonth(state.month == 12);
+					state.month = day.getMonth() + 1;
+				}
+			} else if (state.today == 30) {
+				if (state.month == 4 || state.month == 6 || state.month == 9 || state.month == 11) {
+					state.month -= 1;
+				}
+			} else if (state.today == 29 && state.month == 2) {
 				state.month -= 1;
 			}
 		},
